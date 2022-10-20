0cc330b7172d330803049303eff5cb9d10af4f4b
Alon Sakin <alon.sakin@gmail.com>
2022-10-20 18:41:54 -0400

    Pygame Screen Setup done

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import pygame
+import pygame,sys
 
-#TODO hellovcvxcv
+pygame.init()
+screen = pygame.display.set_mode((750, 750))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.update()
+
+    clock.tick(60)
