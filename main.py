9f8b98e4a2d46533c3bd60fa7cd55c7c894c92fe
Alon Sakin <alon.sakin@gmail.com>
2022-10-27 18:12:30 -0400

    Tile Editing Start

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,x,y,type = "water"):
         super().__init__()
         self.type = type
-        self.image = pygame.surface.Surface((25,25))
+        self.image = pygame.surface.Surface((5,5))
         if self.type == "water":
             self.image.fill("blue")
         elif self.type == "grass":
             self.image.fill("green")
 
-        self.rect = self.image.get_rect(topleft=(x * 25,y * 25))
+        self.rect = self.image.get_rect(topleft=(x * 5,y * 5))
+
+    def update(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]):
+            if(pygame.mouse.get_pressed(3)[0]):
+                self.type = "grass"
+
+
+        if self.type == "water":
+            self.image.fill("blue")
+        elif self.type == "grass":
+            self.image.fill("green")
+
 
 pygame.init()
 screen = pygame.display.set_mode((750, 750))
 clock = pygame.time.Clock()
 tiles = pygame.sprite.Group()
-for x in range(30):
-    for y in range(30):
+for x in range(150):
+    for y in range(150):
         tiles.add(Tile(x,y))
 
 
