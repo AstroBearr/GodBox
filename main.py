2f5b0cf64161bd967312ecafd6c5a9965c75db9c
Alon Sakin <alon.sakin@gmail.com>
2022-10-27 19:07:48 -0400

    Yay Stuf Wurk

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import pygame,sys
 
+ocean_0 = pygame.image.load("Assets/Art/pixil_ocean.png")
+ocean_1 = pygame.image.load("Assets/Art/pixil_ocean2.png")
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self,x,y,type = "water"):
         super().__init__()
         self.type = type
-        self.image = pygame.surface.Surface((5,5))
+        self.image = pygame.surface.Surface((15,15))
         if self.type == "water":
-            self.image.fill("blue")
+            self.image = pygame.image.load("Assets/Art/pixil_ocean.png")
         elif self.type == "grass":
             self.image.fill("green")
+        elif self.type == "stone":
+            self.image = pygame.image.load("Assets/Art/stone.png")
 
-        self.rect = self.image.get_rect(topleft=(x * 5,y * 5))
+        self.rect = self.image.get_rect(topleft=(x * 15,y * 15))
 
     def update(self):
         if self.rect.collidepoint(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]):
             if(pygame.mouse.get_pressed(3)[0]):
                 self.type = "grass"
 
+                if self.type == "water":
+                    self.image = pygame.image.load("Assets/Art/pixil_ocean.png")
+                elif self.type == "grass":
+                    self.image.fill("green")
+                elif self.type == "stone":
+                    self.image = pygame.image.load("Assets/Art/stone.png")
 
-        if self.type == "water":
-            self.image.fill("blue")
-        elif self.type == "grass":
-            self.image.fill("green")
 
 
 pygame.init()
 screen = pygame.display.set_mode((750, 750))
 clock = pygame.time.Clock()
 tiles = pygame.sprite.Group()
-for x in range(150):
-    for y in range(150):
+pygame.mouse.set_visible(False)
+for x in range(50):
+    for y in range(50):
         tiles.add(Tile(x,y))
+pygame.display.set_caption("God Box Beta")
 
 
 while True:
@@ -44,6 +53,8 @@ while True:
     tiles.draw(screen)
 
     tiles.update()
+
+    screen.blit(pygame.image.load("Assets/Art/Mouse.png"), (pygame.mouse.get_pos()[0] - 10, pygame.mouse.get_pos()[1] - 4))
 
     pygame.display.update()
 
