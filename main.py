c301758cf6560f43ff6a5ae178a6b49ddca3fb45
Alon Sakin <alon.sakin@gmail.com>
2022-10-27 18:06:16 -0400

    Yay Tiles Spawn yaay

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pygame,sys
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,x,y,type = "water"):
+        super().__init__()
+        self.type = type
+        self.image = pygame.surface.Surface((25,25))
+        if self.type == "water":
+            self.image.fill("blue")
+        elif self.type == "grass":
+            self.image.fill("green")
+
+        self.rect = self.image.get_rect(topleft=(x * 25,y * 25))
+
 pygame.init()
 screen = pygame.display.set_mode((750, 750))
 clock = pygame.time.Clock()
+tiles = pygame.sprite.Group()
+for x in range(30):
+    for y in range(30):
+        tiles.add(Tile(x,y))
+
 
 while True:
     for event in pygame.event.get():
@@ -10,7 +27,11 @@ while True:
             pygame.quit()
             sys.exit(0)
 
-    screen.fill((0, 0, 0))
+    screen.fill((200, 200, 200))
+
+    tiles.draw(screen)
+
+    tiles.update()
 
     pygame.display.update()
 
